1a381908b9aa38c0292550a21ba50d9131102013
ivanebernal <github@ivanesparza.dev>
2022-12-13 21:03:45 -0800

    Day 13. Improvements

--- 13/part2/distresssignal.kts
+++ 13/part2/distresssignal.kts
@@ -23,44 +23,29 @@ println("Decoder key: $decoderKey")
 
 class LineComparator: Comparator<String> {
     override fun compare(left: String, right: String): Int {
-        if(left.isEmpty() && right.isEmpty()) {
-            return 0
-        } else if (left.isEmpty() && !right.isEmpty()) {
-            return -1
-        } else if (!left.isEmpty() && right.isEmpty()) {
-            return 1
+        if (left.length >= 1 && left[0] != '[' && right.length >= 1 && right[0] != '[') {
+            // Both are integers
+            return left.toInt() - right.toInt()
+        }
+        var leftSubs = if (left.length > 1 && left[0] == '[') {
+            left.substring(1, left.length - 1)
         } else {
-            if (left.length >= 1 && left[0] != '[' && right.length >= 1 && right[0] != '[') {
-                // Both are integers
-                return left.toInt() - right.toInt()
-            }
-
-            var leftSubs = if (left.length > 1 && left[0] == '[') {
-                left.substring(1, left.length - 1)
-            } else {
-                left
-            }
-            var rightSubs = if (right.length > 1 && right[0] == '[') {
-                right.substring(1, right.length - 1)
-            } else {
-                right
-            }
-            val leftItems = getItems(leftSubs)
-            val rightItems = getItems(rightSubs)
-            for (i in 0 until Math.min(leftItems.size, rightItems.size)) {
-                val comparison = compare(leftItems[i], rightItems[i])
-                if (comparison != 0) {
-                    return comparison
-                }
-            }
-            if (leftItems.size > rightItems.size) {
-                return 1
-            } else if (rightItems.size > leftItems.size) {
-                return -1
-            } else {
-                return 0
+            left
+        }
+        var rightSubs = if (right.length > 1 && right[0] == '[') {
+            right.substring(1, right.length - 1)
+        } else {
+            right
+        }
+        val leftItems = getItems(leftSubs)
+        val rightItems = getItems(rightSubs)
+        for (i in 0 until Math.min(leftItems.size, rightItems.size)) {
+            val comparison = compare(leftItems[i], rightItems[i])
+            if (comparison != 0) {
+                return comparison
             }
         }
+        return leftItems.size - rightItems.size
     }
 
     fun getItems(line: String): List<String> {
